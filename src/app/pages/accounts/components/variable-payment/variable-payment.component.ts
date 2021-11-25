ed576b37616ca8af604d490b4bace88283b03531
jaasielmalagon <jaasiel.mendez@alumno.uttehuacan.edu.mx>
2021-11-25 12:16:26 -0600

    variable payment styles and logic added

--- src/app/pages/accounts/components/variable-payment/variable-payment.component.ts
+++ src/app/pages/accounts/components/variable-payment/variable-payment.component.ts
@@ -1,22 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-variable-payment',
   templateUrl: './variable-payment.component.html',
-  styleUrls: ['./variable-payment.component.scss']
+  styleUrls: ['./variable-payment.component.scss'],
 })
 export class VariablePaymentComponent implements OnInit {
-
   public contentLabels = 'accounts.accounts-dashboard.';
   range = new FormGroup({
     start: new FormControl(),
     end: new FormControl(),
   });
 
-  constructor() { }
+  public payments = [
+    [
+      {
+        id: 1,
+        month: 'Julio',
+        amount: 1000000,
+      },
+      {
+        id: 2,
+        month: 'Agosto',
+        amount: 2500000,
+      },
+      {
+        id: 3,
+        month: 'Septiembre',
+        amount: 10000000,
+      },
+    ],
+    [
+      {
+        id: 4,
+        month: 'Octubre',
+        amount: 1000000,
+      },
+      {
+        id: 5,
+        month: 'Noviembre',
+        amount: 2500000,
+      },
+      {
+        id: 6,
+        month: 'Diciembre',
+        amount: 10000000,
+      },
+    ],
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
