0c8e0ea93938a9a2272a0b7e918c5392b6341575
jaasielmalagon <jaasiel.mendez@alumno.uttehuacan.edu.mx>
2021-11-25 18:29:07 -0600

    cell indicators chart

--- src/app/pages/accounts/components/cell-indicators/cell-indicators.component.ts
+++ src/app/pages/accounts/components/cell-indicators/cell-indicators.component.ts
@@ -1,38 +1,37 @@
 import { ChartComponentOptions } from 'src/app/models/ChartOptions';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cell-indicators',
   templateUrl: './cell-indicators.component.html',
   styleUrls: ['./cell-indicators.component.scss'],
+  // host: {
+  //   '(window:resize)': 'onResize($event)',
+  // },
 })
 export class CellIndicatorsComponent implements OnInit {
+  // @HostListener('window:resize', ['onResize($event)'])
   public contentLabels = 'accounts.accounts-dashboard.';
   range = new FormGroup({
     start: new FormControl(),
     end: new FormControl(),
   });
-  public chartProps!: ChartComponentOptions;
-  public payments = [[], []];
+  public chartProps: ChartComponentOptions = {
+    data: [3, 5, 10, 13, 18, 0],
+    colors: ['#7E84D0', '#0595FF', '#5E5A6A', '#7ED1CA', '#93C2CE', '#E5AB4E'],
+    labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun'],
+    chartType: 'line',
+    width: 500,
+    height: 150,
+    class: 'chart-container-line',
+  };
+  public indicators: ChartComponentOptions[] = [this.chartProps];
   constructor() {}
 
-  ngOnInit(): void {
-    this.chartProps = {
-      data: [3, 5, 1, 1, 1, 1],
-      colors: [
-        '#7E84D0',
-        '#0595FF',
-        '#5E5A6A',
-        '#7ED1CA',
-        '#93C2CE',
-        '#E5AB4E',
-      ],
-      labels: [''],
-      chartType: 'line',
-      width: 500,
-      height: 200,
-      class: 'chart-container-line',
-    };
+  ngOnInit(): void {}
+
+  public onResize(event: any): void {
+    console.log(event.target.innerWidth);
   }
 }
