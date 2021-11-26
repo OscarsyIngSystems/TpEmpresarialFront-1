9f355529fa3cfa3333ce8462f461ebea22a3e019
jaasielmalagon <jaasiel.mendez@alumno.uttehuacan.edu.mx>
2021-11-26 11:30:17 -0600

    generic words in translation JSON

--- src/app/models/ChartOptions.ts
+++ src/app/models/ChartOptions.ts
@@ -2,7 +2,9 @@ import { ChartType } from 'chart.js';
 export interface ChartComponentOptions {
   title?: string;
   data: Array<number>;
-  colors: Array<string>;
+  colors:
+    | Array<string>
+    | Array<{ borderColor: string; backgroundColor: string }>;
   labels: Array<string>;
   chartType: ChartType;
   width: number;
