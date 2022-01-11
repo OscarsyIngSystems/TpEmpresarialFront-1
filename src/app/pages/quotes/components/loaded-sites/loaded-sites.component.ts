d8a2ee7d5edf4313fbde56a31748b5c67927a8d6
emeery <gerardoemery@live.com.mx>
2022-01-11 09:47:43 -0600

    service quotes/sales

--- src/app/pages/quotes/components/loaded-sites/loaded-sites.component.ts
+++ src/app/pages/quotes/components/loaded-sites/loaded-sites.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '@angular/compiler';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { InfoDetail } from 'src/app/models/infoDetail';
 import { Sale } from 'src/app/models/sale';
+import { QuotesService } from 'src/app/services/quotes/quotes.service';
 import { DialogLoadSitesComponent } from '../dialogs/dialog-load-sites/dialog-load-sites.component';
 
 @Component({
@@ -15,6 +18,7 @@ export class LoadedSitesComponent implements OnInit {
   public contentLabels = 'quotes.';
   filterParam: FormControl = new FormControl('0');
   lastValue: number;
+  dataSource = new MatTableDataSource();
   infoDetail: Array<InfoDetail> = [
     {
       name: 'Nombre de la cuenta',
@@ -37,150 +41,62 @@ export class LoadedSitesComponent implements OnInit {
       value: 'Sergio Aparicio Contreras',
     },
   ];
-  dataActivityHistory = {
-    columnsShow: [
-      {
-        key: 'check',
-        columnName: this.contentLabels + 'table-loaded-sites.colum1',
-      },
-      {
-        key: 'numberList',
-        columnName: this.contentLabels + 'table-loaded-sites.colum2',
-      },
-      {
-        key: 'site',
-        columnName: this.contentLabels + 'table-loaded-sites.colum3',
-      },
-      {
-        key: 'coverage',
-        columnName: this.contentLabels + 'table-loaded-sites.colum4',
-      },
-      {
-        key: 'accessMedia',
-        columnName: this.contentLabels + 'table-loaded-sites.colum5',
-      },
-      {
-        key: 'action',
-        columnName: this.contentLabels + 'table-loaded-sites.colum6',
-      },
-    ],
-    dataSource: [
-      {
-        numberList: 1,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'Fibra',
-      },
-      {
-        numberList: 2,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'Microonda',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'Fibra',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'Microonda',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'Microonda',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'Microonda',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-      {
-        numberList: 3,
-        site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
-        coverage: 'Con cobertura',
-        accessMedia: 'N/D',
-      },
-    ],
-  };
+
+  columns: string[] = [
+    'check',
+    'numberList',
+    'site',
+    'coverage',
+    'accessMedia',
+    'action',
+  ];
+  // data = [
+  //   {
+  //     numberList: 1,
+  //     site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
+  //     coverage: 'Con cobertura',
+  //     accessMedia: 'Fibra',
+  //   },
+  //   {
+  //     numberList: 2,
+  //     site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
+  //     coverage: 'Con cobertura',
+  //     accessMedia: 'Microonda',
+  //   },
+  //   {
+  //     numberList: 3,
+  //     site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
+  //     coverage: 'Con cobertura',
+  //     accessMedia: 'Fibra',
+  //   },
+  //   {
+  //     numberList: 4,
+  //     site: 'Audi Pedregal Anillo Periférico Boulevard 3539, San Jerónimo Aculco, La Magdalena Contreras, 10400 Ciudad de México, CDMX 19.335048, -99.19840699999',
+  //     coverage: 'Con cobertura',
+  //     accessMedia: 'N/D',
+  //   },
+  // ];
 
   searchData = new FormControl('', Validators.required);
   filteredOptions: Observable<Sale[]> | undefined;
   selectedIdOption = 0;
 
-  constructor(
-    private dlg: MatDialog
-  ) {
+  constructor(private dlg: MatDialog, private chRef: ChangeDetectorRef, private service:QuotesService) {
     this.lastValue = this.filterParam.value;
+    // this.dataSource.data = this.data;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dataSource.filterPredicate = this.filterPredicate
+    this.getData()
+  }
+
+  getData() {
+    this.service.getData().subscribe((data) => {
+      this.dataSource.data = data
+
+    })
+  }
 
   public setSearchId(id: number): void {
     this.selectedIdOption = id;
@@ -189,28 +105,27 @@ export class LoadedSitesComponent implements OnInit {
   public search(): void {
     alert('Buscando... ' + this.searchData.value);
   }
+
   clearSearch() {
     this.selectedIdOption = 0;
     this.searchData.setValue('');
   }
 
-  doFilter(event: any) {
-    if (this.lastValue !== event.target.value) {
-      this.lastValue = event.target.value;
-    }
 
-    else {
-      this.filterParam.setValue(0);
-      this.lastValue = 0;
-    }
+  onFilter(filterValue:string) {
+    this.dataSource.filter = filterValue
   }
 
+  filterPredicate(data:any, filter: string) {
+    let datas = JSON.stringify(data).includes(filter);
+    return datas
+  }
 
   openDialog() {
     this.dlg.open(DialogLoadSitesComponent, {
       height: '300px',
       width: '400px',
-      panelClass: 'custom-dd'
+      panelClass: 'custom-dd',
     });
   }
 }
