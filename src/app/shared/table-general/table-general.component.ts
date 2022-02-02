ff15030610f0d5dd2b75aaaa0ec4c54c2712f262
emeery <gerardoemery@live.com.mx>
2022-02-02 17:06:23 -0600

    cambios quotes received

--- src/app/shared/table-general/table-general.component.ts
+++ src/app/shared/table-general/table-general.component.ts
@@ -169,7 +169,7 @@ export class TableGeneralComponent implements OnInit {
   ngAfterContentInit(): void {
     this.dtOptions = {
       dom: !this.showHeaderTable
-        ? "<'row'<'col-2'i><'col-2 pt-2'l><'col-2 pt-2' <h1>hola</h1> ><'col-6 pt-2'f>>" +
+        ? "<'row'<'col-2'i><'col-2 pt-2'l><'col-6 pt-2'f>>" +
           "<'row'<'col-12'tr>>" +
           "<'row'<'col-12 d-flex justify-content-center'p>>"
         : '<"bottom"t <"d-flex justify-content-center" p>>',
